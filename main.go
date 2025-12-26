be02e520cdb0638fda11b03b35b744d183bb2a35
Fauzan <fauzanfebriann@gmail.com>
2025-12-26 11:34:39 +0700

    refactor(main): change ServerPool connection checking logic

--- main.go
+++ main.go
@@ -10,8 +10,21 @@ import (
 func main() {
 	serverPools := pool.CreateServerPools(8081, 8082, 8083)
 
-	for !serverPools[0].IsAlive() && !serverPools[1].IsAlive() && !serverPools[2].IsAlive() {
-		fmt.Println("Waiting for a pool up and runing")
+	fmt.Println("Waiting for a pool up and runing")
+	for true {
+		isAPoolAlive := false
+
+		for _, serverPool := range serverPools {
+			if serverPool.IsAlive() {
+				isAPoolAlive = true
+				break
+			}
+		}
+
+		if isAPoolAlive {
+			break
+		}
+
 		time.Sleep(2 * time.Second)
 	}
 
